8f39c10390dbc39e8c9b34e225e1d3c1f2321667
171201 <jinalshah5358@gmail.com>
2024-12-16 22:02:54 +0530

    add middleware in store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-
+import { thunk } from "redux-thunk";
 const rootRedux = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootRedux);
+const store = createStore(rootRedux, applyMiddleware(thunk));
 export default store;
